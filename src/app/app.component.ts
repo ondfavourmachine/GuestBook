faee2aea706a4515c9a48acbf741f779ebf8fcd6
micheal <micheal@almondcareers.com>
2019-08-07 11:59:50 +0100

    guest book CRUD done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+
+interface guestBook{
+  name: string,
+  phoneNumber: number
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GuestbookApp';
+  arrOfGuests: Array<guestBook> = []
+  edit: boolean = false;
+  currentIndex: number
+  guestBookForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    phoneNumber: ['', [Validators.required, Validators.minLength(3)]]
+  })
+  constructor(private fb: FormBuilder){}
+
+  addGuestBook(action?: string): void{
+    if(this.guestBookForm.value && !action){
+      this.arrOfGuests.push(this.guestBookForm.value);
+    }
+    else if(action){
+      console.log(action, this.guestBookForm.value);
+      this.edit = false;
+     
+    }
+    this.guestBookForm.reset();
+  }
+
+  updateGuest(guest?: guestBook, index?:number, action?: string): void{
+    if(!action && index){
+      this.edit = true;
+      this.guestBookForm.patchValue(guest);
+      this.currentIndex = index;
+      return
+    }
+    this.arrOfGuests.splice(1, this.currentIndex);
+    this.arrOfGuests.splice(1, 0, this.guestBookForm.value);
+    this.edit = false;
+    this.guestBookForm.reset();
+
+  }
+
+  deleteGuest(index: number, guest?:guestBook){
+    this.arrOfGuests.splice(index, 1);
+  }
+  
+  get guestBookPhone(){
+    return this.guestBookForm.get('phoneNumber');
+  }
 }
